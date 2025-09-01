cc2be7cbf01749a34cab17c50e5723319a828ce4
Nafisha Aslam <nafishaaslam2@gmail.com>
2025-09-01 17:58:18 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,57 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path:'./env'
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+        
+import express from 'express'
+
+const app = express()
+
+    (async () => {
+        try {
+            app.on("error", (error) => {
+                console.log("ERROR", error)
+                throw error
+            })
+            app.listen(process.env.PORT, () => {
+                console.log(`${process.env.PORT}`);
+            })
+        
+    } catch (error) {
+        console.log("ERROR", error)
+        throw err
+    }
+})()
+*/
